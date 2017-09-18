ad4ff429ca5f8734b57137d05e35bd877eb144c7
Avinash1931 <30903209+Avinash1931@users.noreply.github.com>
2017-09-18 15:35:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ this is my content for my friest Avinash Article. this is my content for my frie
  </p>`
   
 };
-function createTempleate (Data) {
+function createTempleate (data) {
     var title = data.title;
     var date = data.date;
     var heading = dts.heading;
@@ -63,7 +63,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res) {
-     res.send(createTemplate(avinashOne));    
+     res.send(createTemplate(articleOne));    
 });
 app.get('/article-two', function(req, res) {
      res.sendFile(path.join(__dirname, 'ui','article-two.html'));   
